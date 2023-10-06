526e79f19514c923f3401cc65c732a83b5c42e9a
JasonWu00 <zehong.wu@macaulay.cuny.edu>
2023-10-06 15:20:38 -0400

    finished second graph

--- dash_dashboard.py
+++ dash_dashboard.py
@@ -18,7 +18,7 @@ import plotly.express as px
 ru_df = pd.read_csv('data/losses_russia.csv')
 ua_df = pd.read_csv('data/losses_ukraine.csv')
 
-# In-place data cleaning
+# In-place data modifying
 
 losses_types = [
     "abandoned",
@@ -30,6 +30,22 @@ losses_types = [
 ru_df[losses_types] = ru_df[losses_types].fillna(0)
 ua_df[losses_types] = ua_df[losses_types].fillna(0)
 
+ru_losses_sum = pd.DataFrame(columns=losses_types)
+ua_losses_sum = pd.DataFrame(columns=losses_types)
+ru_losses_sum.loc[0] = [0,0,0,0,0]
+ua_losses_sum.loc[0] = [0,0,0,0,0]
+for type in losses_types:
+    ru_losses_sum[type].loc[0] = ru_df[type].sum()
+    ua_losses_sum[type].loc[0] = ua_df[type].sum()
+
+ru_losses_sum = ru_losses_sum.transpose()
+ua_losses_sum = ua_losses_sum.transpose()
+ru_losses_sum = ru_losses_sum.reset_index()
+ua_losses_sum = ua_losses_sum.reset_index()
+ru_losses_sum.rename(columns={0: "count"}, inplace=True)
+ua_losses_sum.rename(columns={0: "count"}, inplace=True)
+print(ru_losses_sum)
+print(ua_losses_sum)
 
 # Initialize the app
 app = Dash(__name__)
@@ -57,6 +73,11 @@ app.layout = html.Div([
     html.Div(children='View vehicle loss distribution for:'),
     dcc.RadioItems(options=['Russia', 'Ukraine'], value='Ukraine', id='country-losses-radioitem'),
     dcc.Graph(figure={}, id='loss-graph'),
+    html.Span(children=[
+        "Both sides have similar abandoned/captured/destroyed proportions.",
+        html.Br(),
+        "This indicates that both sides are experiencing similar conditions on the front lines.",
+    ])
 ])
 
 # Add controls to build the interaction
@@ -77,9 +98,9 @@ def update_manufacturers_graph(col_chosen):
 )
 def update_loss_graph(col_chosen):
     if col_chosen == "Russia":
-        figure=px.histogram(ru_df, x="equipment", histfunc='sum')
+        figure=px.histogram(ru_losses_sum, x="index", y="count", histfunc='sum')
     elif col_chosen == "Ukraine":
-        figure=px.histogram(ua_df, x="equipment", histfunc='sum')
+        figure=px.histogram(ua_losses_sum, x="index", y="count", histfunc='sum')
     return figure
 
 # Run the app
