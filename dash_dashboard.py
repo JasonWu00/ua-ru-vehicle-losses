79cd584d627cf2bcfb20e9c97d3d213dbcde7858
JasonWu00 <zehong.wu@macaulay.cuny.edu>
2023-10-06 12:05:00 -0400

    added first dynamic chart

--- dash_dashboard.py
+++ dash_dashboard.py
@@ -10,21 +10,38 @@ This file was made with the reference of the Plotly Dash tutorial:
 https://dash.plotly.com/tutorial
 """
 
-from dash import Dash, html, dash_table
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
+import plotly.express as px
 
 # Incorporate data
-ru_df = pd.read_csv('data/kaggle/losses_russia.csv')
-ua_df = pd.read_csv('data/kaggle/losses_ukraine.csv')
+ru_df = pd.read_csv('data/losses_russia.csv')
+ua_df = pd.read_csv('data/losses_ukraine.csv')
 
 # Initialize the app
 app = Dash(__name__)
 
 # App layout
 app.layout = html.Div([
-    html.Div(children='Placeholder text'),
-    dash_table.DataTable(data=ru_df.to_dict('records'), page_size=10)
+    html.Div(children='Data Science: Week 5 exercise:'),
+    html.Div(children='Exploratory charts using vehicle loss data from the War in Ukraine'),
+    html.Hr(),
+    html.Div(children='View vehicle manufacturer distribution for:'),
+    dcc.RadioItems(options=['Russia', 'Ukraine'], value='Ukraine', id='country-radioitem'),
+    dcc.Graph(figure={}, id='manufacturer-graph')
 ])
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='manufacturer-graph', component_property='figure'),
+    Input(component_id='country-radioitem', component_property='value')
+)
+def update_graph(col_chosen):
+    if col_chosen == "Russia":
+        figure=px.histogram(ru_df, x='manufacturer', y='losses_total', histfunc='sum')
+    elif col_chosen == "Ukraine":
+        figure=px.histogram(ua_df, x='manufacturer', y='losses_total', histfunc='sum')
+    return figure
 
 # Run the app
 if __name__ == '__main__':
